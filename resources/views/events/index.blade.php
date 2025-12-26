6c192323bc00d084ce106ff11f66fac96cfba6ca
Jelle schroeven <jelle.schroeven@student.ehb.be>
2025-12-26 18:24:59 +0100

    Update index.blade.php
    knop voor redirect naar aanmaken event toegevoegt

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -8,6 +8,13 @@
     <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
+                @auth
+                    @if(auth()->user()->is_admin)
+                        <a href="{{ route('events.create') }}" 
+                        class="inline-block bg-blue-600 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded shadow mb-4">Nieuw Event</a>
+                    @endif
+                @endauth
+
                 @if($events->isEmpty())
                     <p>No events found.</p>
                 @else
