3e6487f6db3ec207756657b057b7c582bec9ed0e
Jelle schroeven <jelle.schroeven@student.ehb.be>
2025-12-26 19:58:22 +0100

    Update index.blade.php
    feedback over update/edit/create verdwijnt na 3seconde

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -16,7 +16,12 @@
                 @endauth
 
                 @if(session('success'))
-                    <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mb-4">
+                    <div
+                        x-data="{ show: true }"
+                        x-init="setTimeout(() => show = false, 3000)"
+                        x-show="show" 
+                        class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mb-4"
+                    >
                         {{ session('success') }}
                     </div>
                 @endif
