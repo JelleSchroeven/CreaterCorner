03b9a7cb4d9097d0241bcb67d6ee32ca26a6a0c3
Jelle schroeven <jelle.schroeven@student.ehb.be>
2025-12-26 16:59:33 +0100

    routes toegevoegt
    routes voor product , newspost en events
    
    routes gaan ook via middelware om te checken of user ingelogt is

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+//Controllers
+use App\Http\Controllers\NewsPostController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +20,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('news-posts', NewsPostController::class)->middleware('auth');
+Route::resource('events', EventController::class)->middleware('auth');
+Route::resource('products', ProductController::class)->middleware('auth');
 
 require __DIR__.'/auth.php';
