ed8361af543c6bbb0930f9d93a6c7995081ba9cf
Jelle schroeven <jelle.schroeven@student.ehb.be>
2025-12-27 19:03:45 +0100

    contact email template

---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,7 @@
+<h2>Nieuw contactformulier bericht</h2>
+
+<p><strong>Naam:</strong> {{ $data['name'] }}</p>
+<p><strong>Email:</strong> {{ $data['email'] }}</p>
+<p><strong>Onderwerp:</strong> {{ $data['subject'] }}</p>
+<p><strong>Bericht:</strong></p>
+<p>{{ $data['message'] }}</p>
