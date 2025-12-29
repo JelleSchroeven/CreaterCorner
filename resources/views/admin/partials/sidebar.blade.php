fe8bd7f3e1631775b72d26e8e9847b7d13afca81
Jelle schroeven <jelle.schroeven@student.ehb.be>
2025-12-29 16:11:04 +0100

    aparte layout voor admin aangemaakt

---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,5 @@
+<ul>
+    <li><a href="{{ route('events.index') }}">Events</a></li>
+    <li><a href="{{ route('products.my') }}">Products</a></li>
+    <li><a href="{{ route('shops.index') }}">Shops</a></li>
+</ul>
