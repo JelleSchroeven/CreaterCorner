e28ed81741934564e3eb4d1a7f3c4859b7857a28
Jelle schroeven <jelle.schroeven@student.ehb.be>
2026-01-02 11:06:06 +0100

    Update 2026_01_02_083034_create_news_table.php
    database migratie update, image moet nullable zijn zodat niet elk news bericht een image moet hebben

--- database/migrations/2026_01_02_083034_create_news_table.php
+++ database/migrations/2026_01_02_083034_create_news_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('news', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->text('content');
             $table->timestamp('published_at');
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
