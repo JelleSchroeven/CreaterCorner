340aa12bc83e3e6597c052d384b1b131abcb8574
Jelle schroeven <jelle.schroeven@student.ehb.be>
2026-01-02 09:35:38 +0100

    news model
    news model aangemaakt en ingevult

---
+++ app/Models/News.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    protected $fillable = [
+        'title', 'image, 'content, published_at , user_id 
+    ];
+
+    protected $casts  =[
+        'publishedd_at' => 'datetime'
+    ]
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id')
+    }
+}
