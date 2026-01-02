f966d78b4870bb1e2a8a7f4c9d9e0d64b4bc32a1
Jelle schroeven <jelle.schroeven@student.ehb.be>
2026-01-02 09:41:21 +0100

    public news controller update

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class NewsController extends Controller
 {
-    //
+    public function index()
+    {
+        $news = News::latest('published_at')->paginate(10);
+        return view('news.index', compact('news'));
+    }
+
+    public function show(News $news)
+    {
+        return view('news.show', compact('news'));
+    }
+
 }
