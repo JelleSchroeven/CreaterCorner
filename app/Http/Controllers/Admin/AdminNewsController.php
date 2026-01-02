0929a97b8046b5008fcdd19f3e7a61697cb177c9
Jelle schroeven <jelle.schroeven@student.ehb.be>
2026-01-02 09:59:15 +0100

    Update AdminNewsController.php
    strore , update, destroy functies toegevoegtt

--- app/Http/Controllers/Admin/AdminNewsController.php
+++ app/Http/Controllers/Admin/AdminNewsController.php
@@ -4,62 +4,73 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Models\News
 
 class AdminNewsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'title' =>'recuired|max:255',
+            'image' =>'recuired|image|max:2048',
+            'content' => 'recuired',
+            'published_at' => 'recuired|data'
+        ]);
+
+        $date['image'] = $request->file('image')->store('news', 'public');
+        $data['user_id'] = Auth() -> id();
+
+        News::create($data);
+        return redirect()->route('admin.news.index')
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        //
+        $data = $request->validate([
+            'title' => 'required|max:255',
+            'image' => 'nullable|image|max:2048',
+            'content' => 'required',
+            'published_at' => 'required|date'
+        ]);
+
+        if ($request->hasFile('image')) {
+            Storage::disk('public')->delete($news->image);
+            $data['image'] = $request->file('image')->store('news', 'public');
+        }
+
+        $news->update($data);
+
+        return redirect()->route('admin.news.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+        if ($news->image && Storage::disk('public')->exists($news->image)) {
+        Storage::disk('public')->delete($news->image);
+        }
+
+        $news -> delete()
+        return redirect()->route('admin.news.index')
     }
 }
