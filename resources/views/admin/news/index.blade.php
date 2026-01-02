862d99bc666b4f1a899ec80689eb3b40eaa9f52f
Jelle schroeven <jelle.schroeven@student.ehb.be>
2026-01-02 10:39:29 +0100

    Update index.blade.php
    pagina gecentreerd en max 80% breedte

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -7,45 +7,46 @@
             </a>
         </div>
     </x-slot>
-
-    <div class="overflow-x-auto mt-4">
-        <table class="w-full bg-white shadow rounded">
-            <thead>
-                <tr class="border-b bg-gray-100">
-                    <th class="p-3 text-left">Titel</th>
-                    <th class="p-3 text-center">Datum</th>
-                    <th class="p-3 text-center">Acties</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($news as $item)
-                    <tr class="border-b hover:bg-gray-50">
-                        <td class="p-3">{{ $item->title }}</td>
-                        <td class="p-3 text-center">
-                            {{ $item->published_at->format('d-m-Y') }}
-                        </td>
-                        <td class="p-3 text-center space-x-2">
-                            <a href="{{ route('admin.news.show', $item) }}" class="text-blue-600">Bekijk</a>
-                            <a href="{{ route('admin.news.edit', $item) }}" class="text-yellow-600">Bewerk</a>
-
-                            <form class="inline"
-                                  method="POST"
-                                  action="{{ route('admin.news.destroy', $item) }}"
-                                  onsubmit="return confirm('Weet je zeker dat je dit item wilt verwijderen?');">
-                                @csrf
-                                @method('DELETE')
-                                <button class="text-red-600">
-                                    Verwijder
-                                </button>
-                            </form>
-                        </td>
+    <div class="flex justify-center mt-4">
+        <div class="w-full max-w-[80%] overflow-x-auto">
+            <table class="w-full bg-white shadow rounded">
+                <thead>
+                    <tr class="border-b bg-gray-100">
+                        <th class="p-3 text-left">Titel</th>
+                        <th class="p-3 text-center">Datum</th>
+                        <th class="p-3 text-center">Acties</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach($news as $item)
+                        <tr class="border-b hover:bg-gray-50">
+                            <td class="p-3">{{ $item->title }}</td>
+                            <td class="p-3 text-center">
+                                {{ $item->published_at->format('d-m-Y') }}
+                            </td>
+                            <td class="p-3 text-center space-x-2">
+                                <a href="{{ route('admin.news.show', $item) }}" class="text-blue-600">Bekijk</a>
+                                <a href="{{ route('admin.news.edit', $item) }}" class="text-yellow-600">Bewerk</a>
 
-        <div class="mt-4">
-            {{ $news->links() }}
+                                <form class="inline"
+                                    method="POST"
+                                    action="{{ route('admin.news.destroy', $item) }}"
+                                    onsubmit="return confirm('Weet je zeker dat je dit item wilt verwijderen?');">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="text-red-600">
+                                        Verwijder
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+            <div class="mt-4">
+                {{ $news->links() }}
+            </div>
         </div>
     </div>
 </x-app-layout>
