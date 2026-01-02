193464111d950f4f9b26255dc40411a99a8d357b
Jelle schroeven <jelle.schroeven@student.ehb.be>
2026-01-02 11:20:49 +0100

    Update create.blade.php
    layout changes

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -3,35 +3,39 @@
         <h2 class="text-xl font-semibold">Nieuw nieuwsitem</h2>
     </x-slot>
 
-    <form method="POST"
-          action="{{ route('admin.news.store') }}"
-          enctype="multipart/form-data"
-          class="space-y-6 max-w-xl">
+    <div class='flex justify-center mt-4'>
+        <div class="max-w-[80vw] space-y-6">
+            <form method="POST"
+                action="{{ route('admin.news.store') }}"
+                enctype="multipart/form-data"
+                class="w-full bg-white shadow rounded-lg p-6 space-y-6">
 
-        @csrf
+                @csrf
 
-        <div>
-            <label class="block font-medium">Titel</label>
-            <input class="w-full border rounded p-2" type="text" name="title" value="{{ old('title') }}">
+                <div>
+                    <label class="block font-medium mb-1" for='title'>Titel</label>
+                    <input class="w-full border rounded p-2" id='title' type="text" name="title" value="{{ old('title') }}">
+                </div>
+
+                <div>
+                    <label class="block font-medium mb-1" for='image'>Afbeelding</label>
+                    <input id='image' type="file" name="image" class="block">
+                </div>
+
+                <div>
+                    <label class="block font-medium mb-1" for='content'>Inhoud</label>
+                    <textarea class="w-full border rounded p-2" id='content' name="content" rows="6">{{ old('content') }}</textarea>
+                </div>
+
+                <div>
+                    <label class="block font-medium mb-1" for='publication_date'>Publicatiedatum</label>
+                    <input class="border rounded p-2" id='publication_date' type="date" name="published_at" value="{{ old('published_at') }}">
+                </div>
+
+                <button class="px-5 py-2 bg-blue-600 text-white rounded">
+                    Opslaan
+                </button>
+            </form>
         </div>
-
-        <div>
-            <label class="block font-medium">Afbeelding</label>
-            <input type="file" name="image" class="block">
-        </div>
-
-        <div>
-            <label class="block font-medium">Inhoud</label>
-            <textarea class="w-full border rounded p-2" name="content" rows="6">{{ old('content') }}</textarea>
-        </div>
-
-        <div>
-            <label class="block font-medium">Publicatiedatum</label>
-            <input class="border rounded p-2" type="date" name="published_at" value="{{ old('published_at') }}">
-        </div>
-
-        <button class="px-5 py-2 bg-blue-600 text-white rounded">
-            Opslaan
-        </button>
-    </form>
+    </div>
 </x-app-layout>
